ed73f2d92d86ad869ba1ca4990b2580ed1294f17
Rishi2600 <rishiraj6177@gmail.com>
2025-02-23 22:46:05 +0530

    some changes of loops

--- Notes.js
+++ Notes.js
@@ -491,5 +491,27 @@ function setTimeoutPromisified(ms) {                            --> This part he
   }
   
   R.then(callback);
+
+
+  //while loop, for loop & the difference of it from a for loop
+
+  //for loop
+  for (let i=0; i<5; i++) {
+    console.log(i);
+  }
+
+  //while loop
+  let i=0;
+  while(i<5) {
+    console.log(i);
+    i++
+  }
+
+  //do while loop
+  let i=0;
+  do {
+    console.log(i);
+    i++
+  } while(i<5)
                                                                       */
   
